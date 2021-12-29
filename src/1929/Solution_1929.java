3d5694217df833d0f53da343a03e829fd3a1150a
sajan-daniel <86237573+sajan-daniel@users.noreply.github.com>
2021-12-29 11:39:00 +0530

    new solution added

---
+++ src/1929/Solution_1929.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Solution_1929 {
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+
+        for(int i=0; i<n; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,1};
+        int[] ans = getConcatenation(nums);
+        System.out.println(Arrays.toString(ans));
+
+    }
+}
